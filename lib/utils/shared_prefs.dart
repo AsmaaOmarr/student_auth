25f7e5d5940db7159e5c616a1d8588b26ca63e04
Asmaa omar <119797320+AsmaaOmarr@users.noreply.github.com>
2024-04-06 15:56:02 +0200

    .

--- lib/utils/shared_prefs.dart
+++ lib/utils/shared_prefs.dart
@@ -3,20 +3,15 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../models/user.dart';
 
 class SharedPreferencesService {
+
   static const String _keyUserList = 'userList';
 
-  // Add a user to the list of users
-  static Future<void> addUser(User user) async {
-    List<User> userList = await getAllUsers();
-    userList.add(user);
-    await _saveUserList(userList);
-  }
-
-  // Remove a user from the list of users
-  static Future<void> removeUser(String email) async {
-    List<User> userList = await getAllUsers();
-    userList.removeWhere((user) => user.email == email);
-    await _saveUserList(userList);
+  // Private method to save the list of users
+  static Future<void> _saveUserList(List<User> userList) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> userStrings =
+        userList.map((user) => json.encode(user.toJson())).toList();
+    await prefs.setStringList(_keyUserList, userStrings);
   }
 
   // Get all users from the list
@@ -31,6 +26,19 @@ class SharedPreferencesService {
         .toList();
   }
 
+  // Add a user to the list of users
+  static Future<void> addUser(User user) async {
+    List<User> userList = await getAllUsers();
+    userList.add(user);
+    await _saveUserList(userList);
+  }
+
+  // Remove a user from the list of users
+  static Future<void> removeUser(String email) async {
+    List<User> userList = await getAllUsers();
+    userList.removeWhere((user) => user.email == email);
+    await _saveUserList(userList);
+  }
   // Check if the email exists in the list of users
   static Future<bool> doesEmailExist(String email) async {
     List<User> userList = await getAllUsers();
@@ -43,14 +51,6 @@ class SharedPreferencesService {
     List<User> userList = await getAllUsers();
     return userList
         .any((user) => user.email == email && user.password == password);
-  }
-
-  // Private method to save the list of users
-  static Future<void> _saveUserList(List<User> userList) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String> userStrings =
-        userList.map((user) => json.encode(user.toJson())).toList();
-    await prefs.setStringList(_keyUserList, userStrings);
   }
 
   // Method to get a user by email
